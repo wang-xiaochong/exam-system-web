748c38ba5dea8e1bea6dda563b4ecc3b9c8b46b9
xiaoshuai <1378334435@qq.com>
2021-12-31 19:21:06 +0800

    init W

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Student from '@/components/Student'
+import Teacher from '@/components/Teacher'
+import Login from '@/views/login/index'
 
 Vue.use(Router)
 
@@ -8,8 +10,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/student',
+      name: 'Student',
+      component: Student,
+      children: [
+        {
+          path: '/student',
+          name: 'Student',
+          component: Student,
+        }
+      ]
+    },
+    {
+      path: '/teacher',
+      name: 'Teacher',
+      component: Teacher
+    },
+
+
+
+  ],
+
 })
